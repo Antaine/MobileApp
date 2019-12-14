d71c4b910683d5003df917ef7bd4ea9cd347907c
Antaine <aconn@outlook.com>
2019-12-14 23:47:10 +0000

    Level 2 & Finish

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ 
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D co)
     {
-        
+         if (co.tag == "Player")
+          {
+            Debug.Log("Test");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        else
+        {
+            Debug.Log("Failed " + co.tag);
+        }
     }
 }
