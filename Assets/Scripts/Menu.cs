952d354c1ebe61dc8572be3c0e3d258865e642a6
Antaine <aconn@outlook.com>
2019-12-14 21:39:31 +0000

    Menu to level

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string level = "level1";
+    public void PlayGame ()
+    {
+      //  Application.LoadLevel(level);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+
+    }
+
+    public void QuityGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        //
+      //  Application.Quit();
+
+    }
+}
---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform player;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+   // RectOffset = -4;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset.z = (transform.position.z - 1);
+        offset.x = (transform.position.x) + 1;
+        offset.y = (transform.position.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+     //   yaw += speedH * Input.GetAxis("Mouse X");
+     //   pitch -= speedV * Input.GetAxis("Mouse Y");
+
+    }
+}
